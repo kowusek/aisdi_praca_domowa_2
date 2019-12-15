3e74f77931b911b4c8a46c31bcffe4b0f809ccfa
mkowals7 <kowusek@gmail.com>
2019-12-16 00:46:13 +0100

    poprawienie algorytmu sortowania

--- deck.cpp
+++ deck.cpp
@@ -79,7 +79,7 @@ void deck::selectionSort() {
     while ( temp ) {
 
         Node* min = temp;
-        Node* b = temp->next;
+        Node* b = temp;
 
         while ( b ) {
 
@@ -89,10 +89,51 @@ void deck::selectionSort() {
             b = b->next;
         }
 
-        card x = temp->data;
-        temp->data = min->data;
-        min->data = x;
+        card y = min->data;
+
+        b = temp;
+
+
+        card z = b->data;
+        card v = b->data;
+
+        while (b->next && b != min) {
+
+            v = z;
+            z = b->next->data;
+            b->next->data = v;
+            b = b->next;
+        }
+        temp->data = y;
         temp = temp->next;
+    }
+
+}
+
+void stableSelectionSort(int a[], int n)
+{
+    // Iterate through array elements
+    for (int i = 0; i < n - 1; i++)
+    {
+
+        // Loop invariant : Elements till a[i - 1]
+        // are already sorted.
+
+        // Find minimum element from
+        // arr[i] to arr[n - 1].
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+            if (a[min] > a[j])
+                min = j;
+
+        // Move minimum element at current i.
+        int key = a[min];
+        while (min > i)
+        {
+            a[min] = a[min - 1];
+            min--;
+        }
+        a[i] = key;
     }
 }
 
